f4ec285ec4eeb12f31db7bf067c21bfc9eb4f3b0
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-05-11 18:49:36 +0200

    fixed all bugs

--- cpptree.h
+++ cpptree.h
@@ -107,6 +107,12 @@ class ftree {
                     if (matrix[i][j] == '.' && j == maxdepth-1) matrix[i][j] = 'x';
                 }
             }
+            for (int i = 0; i < sorted.size(); i++) {
+                for (int j = 0; j < maxdepth; j++) {
+                    cout << matrix[i][j];
+                }
+                cout << "\n";
+            }
             int exs = 0;
             int nypos = 0;
             for (int i = 0; i < sorted.size(); i++) {
@@ -127,13 +133,25 @@ class ftree {
             }
             for (int i = 0; i < sorted.size(); i++) {
                 for (int j = 0; j < maxdepth; j++) {
+                    cout << matrix[i][j];
+                }
+                cout << "\n";
+            }
+            for (int i = 0; i < sorted.size(); i++) {
+                for (int j = 0; j < maxdepth; j++) {
                     if (matrix[i][j] == 'x' ) {
                         if (matrix[i][j+1] != 'x' && matrix[i+1][j] == 'x') matrix[i][j] = '+';
                         if (matrix[i][j+1] != 'x' && matrix[i+1][j] != 'x') matrix[i][j] = 'L';
-                        if (matrix[i][j+1] == 'x' || matrix[i][j+1] == '.') matrix[i][j] = '|';
+                        if (j != maxdepth-1)
+                            if ((matrix[i][j+1] == 'x' || matrix[i][j+1] == '.') && matrix[i+1][j] != ' ') matrix[i][j] = '|';
                     }
-                    //else if (matrix[i][j] == '.') matrix[i][j] = '.';
                 }
+            }
+            for (int i = 0; i < sorted.size(); i++) {
+                for (int j = 0; j < maxdepth; j++) {
+                    cout << matrix[i][j];
+                }
+                cout << "\n";
             }
             cout << "0 - " << _names.at(0) << "\n";
             for (int i = 0; i < sorted.size(); i++) {
