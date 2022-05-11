172409c6d199a3ae809c4146fa71536b0c2faeef
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-05-11 17:26:35 +0200

    Add files via upload

--- cpptree.cpp
+++ cpptree.cpp
@@ -164,15 +164,3 @@ class ftree {
         }
 
 };
-
-int main() {
-    ftree<string> mytree;
-    mytree.addNode(0); // Add to node 0
-    mytree.addNode(0);
-    int nodenum = mytree.addNode(2); // Return value is the new nodes number
-    int new_nodenum = mytree.addNode(nodenum); // Add a new node to the node added to 2
-    mytree.setName(nodenum, "Some string here"); // Set the value of nodenum (the node added to 2) to a string (or other data type)
-    mytree.removeNode(new_nodenum);
-
-    mytree.printTree();
-}
