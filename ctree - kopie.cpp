ca923e682d71f7475d250cd6d3f731b0904dd87c
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-05-12 14:00:11 +0200

    Add files via upload

---
+++ ctree - kopie.cpp
@@ -0,0 +1,241 @@
+﻿#ifndef _cpptree_h_
+#define _cpptree_h_
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using std::vector;
+using std::string;
+using std::cout;
+
+template <class T>
+class cpptree {
+public:
+    cpptree() {
+        _relations.push_back(-1);
+        _names.push_back(T());
+    }
+
+
+    unsigned int addNode(unsigned int parent) {
+        _relations.push_back(parent);
+        _names.push_back(T());
+        return _relations.size() - 1;
+    }
+
+    unsigned int getParent(unsigned int node) {
+        return _relations.at(node);
+    }
+
+    vector<unsigned int> getChildren(int parent) {
+        vector<unsigned int> found;
+        found = _getOccurences(parent);
+        return found;
+    }
+
+    T getName(unsigned int node) {
+        return _names.at(node);
+    }
+
+    vector<unsigned int> getAllChildren(unsigned int parent) {
+        vector<unsigned int> found;
+        found = _getOccurences(parent);
+        int i = 0;
+        while (i < found.size()) {
+            vector<unsigned int> nfound;
+            nfound = _getOccurences(found.at(i));
+            found.insert(found.end(), nfound.begin(), nfound.end());
+            ++i;
+        }
+        return found;
+    }
+
+    void setName(unsigned int node, T desc) {
+        _names.at(node) = desc;
+    }
+
+    void removeNode(unsigned int node) { // Removing nodes in this vector implementation is _very_ slow
+        vector<unsigned int> found = getAllChildren(node);
+        for (int i = 0; i < found.size(); i++) {
+            _relations.erase(_relations.begin() + found.at(i));
+            for (int j = 0; j < _relations.size(); j++) {
+                if (_relations.at(j) > found.at(i)) {
+                    _relations.at(j)--;
+                }
+            }
+            for (int j = 0; j < found.size(); j++) {
+                if (found.at(j) > found.at(i)) {
+                    found.at(j)--;
+                }
+            }
+            found.erase(found.begin() + i);
+        }
+        /*while (found.size() != 0) {
+            _rLastNode(found.at(found.size() - 1));
+            found = getAllChildren(node);
+        }
+        _rLastNode(node);*/
+    }
+
+    void printTree() { // This implementation of printing the tree is a bit dumb... but it's fast enough so WONTFIX
+        unsigned int maxdepth = 0;
+        for (int i = 0; i < _relations.size(); i++) if (_getDepth(i) > maxdepth) maxdepth = _getDepth(i);
+        vector<unsigned int> sorted = _sortedCrawl();
+
+        char** matrix = new char* [sorted.size() + 1];
+        for (int i = 0; i < sorted.size() + 1; ++i)
+            matrix[i] = new char[maxdepth + 1];
+
+        for (int i = 0; i < sorted.size(); i++) {
+            for (int j = 0; j < maxdepth; j++) {
+                if (j < _getDepth(sorted[i]))
+                    matrix[i][j] = '.';
+                else
+                    matrix[i][j] = ' ';
+            }
+        }
+        for (int i = 0; i < sorted.size(); i++) {
+            for (int j = 0; j < maxdepth; j++) {
+                if (matrix[i][j] == '.' && matrix[i][j + 1] == ' ') matrix[i][j] = 'x';
+                if (matrix[i][j] == '.' && j == maxdepth - 1) matrix[i][j] = 'x';
+            }
+        }
+        int exs = 0;
+        int nypos = 0;
+        for (int i = 0; i < sorted.size(); i++) {
+            for (int j = 0; j < maxdepth; j++) {
+                if (matrix[i][j] == 'x') {
+                    for (int k = i + 1; k < sorted.size(); k++) {
+                        if (matrix[k][j] != '.' && matrix[k][j] != 'x') break;
+                        if (matrix[k][j] == 'x') exs++;
+                    }
+                }
+                nypos = i + 1;
+                while (exs != 0) {
+                    if (matrix[nypos][j] == '.') matrix[nypos][j] = 'x';
+                    else if (matrix[nypos][j] == 'x') --exs;
+                    nypos++;
+                }
+            }
+        }
+        for (int i = 0; i < sorted.size(); i++) {
+            for (int j = 0; j < maxdepth; j++) {
+                if (matrix[i][j] == 'x') {
+                    if (matrix[i][j + 1] != 'x' && matrix[i + 1][j] == 'x') matrix[i][j] = '+';
+                    if (matrix[i][j + 1] != 'x' && matrix[i + 1][j] != 'x') matrix[i][j] = 'L';
+                    if (j != maxdepth - 1)
+                        if ((matrix[i][j + 1] == 'x' || matrix[i][j + 1] == '.') && matrix[i + 1][j] != ' ') matrix[i][j] = '|';
+                }
+            }
+        }
+        cout << "0 - " << _names.at(0) << "\n";
+        for (int i = 0; i < sorted.size(); i++) {
+            for (int j = 0; j < maxdepth; j++) {
+                if (matrix[i][j] == '+') cout << "├── ";
+                else if (matrix[i][j] == 'L') cout << "└── ";
+                else if (matrix[i][j] == '|') cout << "│   ";
+                else if (matrix[i][j] == '.') cout << "    ";
+            }
+            cout << sorted.at(i) << " - " << _names.at(sorted.at(i));
+            cout << "\n";
+        }
+        for (int i = 0; i < sorted.size() + 1; ++i)
+            delete[] matrix[i];
+        delete[] matrix;
+    }
+
+private:
+    vector<int> _relations;
+    vector<T> _names;
+
+
+    vector<unsigned int> _getOccurences(unsigned int parent) {
+        vector<unsigned int> _found;
+        for (int i = 0; i < _relations.size(); i++) {
+            if (_relations.at(i) == parent)
+                _found.push_back(i);
+        }
+        return _found;
+    }
+
+    void _rLastNode(unsigned int node) {
+        _relations.erase(_relations.begin() + node);
+        _names.erase(_names.begin() + node);
+        for (int i = 1; i < _relations.size(); i++) {
+            if (_relations.at(i) > node) _relations.at(i)--;
+        }
+    }
+
+
+    unsigned int _getDepth(unsigned int node) {
+        int parent = getParent(node);
+        int depth = 0;
+        while (parent != -1) {
+            parent = getParent(parent);
+            depth++;
+        }
+        return depth;
+    }
+
+    vector<unsigned int> _sortedCrawl() { // This function doesn't scale well, only a problem when thousands of nodes are reached.
+        int counter = 0;
+        vector<unsigned int> sorted;
+        vector<unsigned int> indexes;
+        indexes.push_back(0);
+        while (true) {
+            vector<unsigned int> found = _getOccurences(counter);
+            if (found.size() > indexes.at(_getDepth(counter))) {
+                counter = found.at(indexes.at(_getDepth(counter)));
+                sorted.push_back(counter);
+                if (indexes.size() > _getDepth(counter) + 1) {
+                    indexes.at(_getDepth(counter) + 1) = 0;
+                    for (int i = _getDepth(counter); i < indexes.size(); i++) {
+                        indexes.at(i) = 0;
+                    }
+                }
+                else
+                    indexes.push_back(0);
+            }
+            else {
+                counter = _relations[counter];
+                if (counter == -1) break;
+                indexes.at(_getDepth(counter)) += 1;
+            }
+        }
+        return sorted;
+    }
+
+};
+#endif
+
+int main() {
+    cpptree<std::string> mytree;
+    mytree.addNode(0);
+    mytree.addNode(1);
+    mytree.addNode(2);
+    mytree.addNode(3);
+    mytree.addNode(3);
+    mytree.addNode(2);
+    mytree.addNode(3);
+    mytree.addNode(7);
+    mytree.addNode(7);
+    mytree.addNode(8);
+    mytree.addNode(2);
+    mytree.addNode(2);
+    mytree.addNode(9);
+    mytree.addNode(13);
+    mytree.addNode(9);
+    mytree.addNode(6);
+    mytree.addNode(16);
+    mytree.addNode(13);
+    mytree.addNode(16);
+    mytree.addNode(17);
+    mytree.addNode(16);
+    mytree.addNode(17);
+    mytree.addNode(13);
+    mytree.addNode(23);
+    mytree.addNode(24);
+    mytree.setName(25, "wew lad");
+    mytree.removeNode(6);
+    mytree.printTree();
+}
